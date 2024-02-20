74c193041de4c8ba307bfe8927f3cd4ccc4166cb
ryan-saloma <102826487+ryan-saloma@users.noreply.github.com>
2024-02-20 21:22:06 +0000

    added an example bin2dec conversion

--- bin2dec-app/app.js
+++ bin2dec-app/app.js
@@ -13,10 +13,12 @@ form.addEventListener('submit', function(e) {
 });
 
 const convertNumber = function(binNum, decNum) {
-    if(binNum.value == '' && decNum.value == '') {
+    let binNumEmpty = binNum.value == '';
+    let decNumEmpty = decNum.value == '';
+    if(binNumEmpty && decNumEmpty) {
         alert('Please enter a binary string or decimal number.')
         return ['',''];
-    } else if( binNum.value == '') {
+    } else if(binNumEmpty) {
         return convertDecimal2Binary(binNum, decNum);
     } else {
         return convertBinary2Decimal();
@@ -24,12 +26,15 @@ const convertNumber = function(binNum, decNum) {
 
 };
 
+
 const convertDecimal2Binary = function(binNum, decNum) {
     let num = decNum.value;
     let binStr = '';
 
     return ['bin', binStr];
 };
+
+// Example: '101010' = 0*2^0 + 1*2^1 + 0*2^2 + 1*2^3 + 0*2^4 + 1*2^5 = 42
 
 const convertBinary2Decimal = function(binNum, decNum) {
     let num = 0;
