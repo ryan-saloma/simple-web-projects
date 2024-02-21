b60a8426591e648a0b34708cf745f8ec4409be69
ryan-saloma <102826487+ryan-saloma@users.noreply.github.com>
2024-02-21 19:07:59 +0000

    added bin2dec conversion

--- bin2dec-app/app.js
+++ bin2dec-app/app.js
@@ -1,44 +1,43 @@
 const form = document.querySelector('#num-input');
-const binNum = document.querySelector('#bin-num');
-const decNum = document.querySelector('#dec-num');
+const convertFrom = document.querySelector('#unit1');
+const convertTo = document.querySelector('#unit2');
+const inputNum = document.querySelector('#input-number');
+const outputNum = document.querySelector('#output-number');
+
+form.addEventListener('reset', function(e) {
+    e.preventDefault();
+    convertFrom.value = 'Binary';
+    convertTo.value = 'Decimal'
+    inputNum.value = '';
+    outputNum.value = '';
+});
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
-    [type, str] =  convertNumber(binNum, decNum);
-    if(type == 'bin')  {
-
-    } else if(type == 'dec') {
-
-    } 
+    outputNum.value = convertNumber();
 });
 
-const convertNumber = function(binNum, decNum) {
-    let binNumEmpty = binNum.value == '';
-    let decNumEmpty = decNum.value == '';
-    if(binNumEmpty && decNumEmpty) {
-        alert('Please enter a binary string or decimal number.')
-        return ['',''];
-    } else if(binNumEmpty) {
-        return convertDecimal2Binary(binNum, decNum);
-    } else {
-        return convertBinary2Decimal();
-    }
-
+const convertNumber = function() {
+    if(convertFrom.value == 'Binary' && convertTo.value == 'Decimal') {
+        return parseInt(inputNum.value, 2);
+    } 
+    return 999;
 };
 
 
 const convertDecimal2Binary = function(binNum, decNum) {
-    let num = decNum.value;
-    let binStr = '';
 
-    return ['bin', binStr];
 };
 
 // Example: '101010' = 0*2^0 + 1*2^1 + 0*2^2 + 1*2^3 + 0*2^4 + 1*2^5 = 42
+// 1978 = 8*10^0 + 7*10^1 + 9*10^2 + 1*10^3
+// 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, B, C, D, E, F
+// 2A = 10*16^0 + 2*16^1
 
-const convertBinary2Decimal = function(binNum, decNum) {
+const convertBinary2Decimal = function() {
     let num = 0;
+    let exp = 0;
     let binStr = binNum.value;
 
-    return ['dec', binStr];
+
 };
