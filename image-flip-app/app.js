76e7442d882835be4d31517c5da6abc2cdca06f8
ryan-saloma <102826487+ryan-saloma@users.noreply.github.com>
2024-03-01 01:07:00 +0000

    added api calls for images

---
+++ image-flip-app/app.js
@@ -0,0 +1,3 @@
+let img1 = fetch("http://placekitten.com/500/500").catch(() => "img/cat1.jpg");
+let img2 = fetch("http://placekitten.com/400/400").catch(() => "img/cat2.jpg");
+let img3 = fetch("http://placekitten.com/400/400").catch(() => "img/cat2.jpg");
